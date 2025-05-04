5f1837a41131a1e40c50c2f2bff418359caceb10
duochq77 <duochq77@gmail.com>
2025-05-04 22:49:53 +0700

    Fix errors in token handling and missing module types

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 // src/main.tsx
+
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
+import SellerPage from './pages/SellerPage';
 
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  createRoot(rootElement).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/seller" element={<SellerPage />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
